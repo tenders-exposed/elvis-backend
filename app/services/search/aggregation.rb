bf3fe0eb0e873d50018733e538909bfc4b0b08d8
georgiana-b <bere.georgi@gmail.com>
2015-12-24 21:31:37 +0200

    Refined aggregation

--- app/services/search/aggregation.rb
+++ app/services/search/aggregation.rb
@@ -1,11 +1,11 @@
 class Search::Aggregation
   attr_accessor :subject, :name, :agg, :type, :embedded_agg
 
-  def initialize(field, type = "terms",embedded_agg = nil)
+  def initialize(field, embedded_agg = nil, options={})
     @subject = field
     @embedded_agg = embedded_agg
     @name = field_name(field)
-    @type = type
+    @type = options[:type] ? options[:type].to_sym : :terms
     @agg =  { aggs: {} }
     build_aggregation
   end
@@ -22,23 +22,32 @@ class Search::Aggregation
   def basic_aggregation
     normal_aggregation = {
       @name=> {
-        @type=> {
+        @type => {
           field: @subject
         }
       }
     }
-    normal_aggregation[@name][@type][:size] = 0 if @type == "terms"
-    normal_aggregation[@name][:aggs] = @embedded_agg.agg[:aggs] if @embedded_agg
+    normal_aggregation[@name][@type][:size] = 0 if @type == :terms
+    normal_aggregation[@name][:aggs] = reverse_nested_aggregation if @embedded_agg
     normal_aggregation
   end
 
   def nested_field_aggregation
-    nested_aggregation = {
+    nested_agg = {
       collection_name => {
         nested: {
           path: nested_path(subject)
         },
         aggs: basic_aggregation
+      }
+    }
+  end
+
+  def reverse_nested_aggregation
+    reverse_nested_agg = {
+      reversed_agg_name => {
+        reverse_nested: {},
+        aggs:  @embedded_agg.agg[:aggs]
       }
     }
   end
@@ -61,4 +70,7 @@ class Search::Aggregation
     field_name(nested_path(@subject))
   end
 
+  def reversed_agg_name 
+    ('back_for_' + @embedded_agg.collection_name.to_s).to_sym
+  end
 end
--- app/services/search/aggregation_parser.rb
+++ app/services/search/aggregation_parser.rb
@@ -1,35 +1,28 @@
 class Search::AggregationParser
-  attr_accessor :agg, :response
+  attr_accessor :response
 
-  def initialize(q, agg)
-    @agg = agg
-    @response = Search::ContractSearch.new(q, @agg).request.raw_response['aggregations'].deep_symbolize_keys
+  def initialize(response)
+    @response = response['aggregations'].deep_symbolize_keys
   end
 
-  def get_results
-    result = parse_response(@response, @agg.collection_name, @agg.name)
-    if embedded?
-      embedded = result.except(:results)
-      embedded[:results] = result[:results].map do |res|
-        parse_response(res, @agg.embedded_agg.collection_name, @agg.embedded_agg.name)
+  def parse_response
+    @response.extend Hashie::Extensions::DeepFind
+    pe = @response.deep_find(:buckets)
+    pes = pe.map do |pe|
+      pe.extend  Hashie::Extensions::DeepFind
+      pe[:results] = pe.deep_find(:buckets)
+      pe[:value] = pe.deep_find(:value)
+      if pe[:results]
+        pe[:results].map! do |child|
+          child.extend Hashie::Extensions::DeepFind
+          child[:value] = child.deep_find(:value)
+          child.slice(:key, :doc_count, :value)
+        end
+        pe.slice(:key, :doc_count, :results)
+      else
+        pe.slice(:key, :doc_count, :value)
       end
-      embedded
-    else
-      result
     end
   end
-
-  def embedded?
-    return true if @agg.embedded_agg
-    return false
-  end
-
-  def parse_response(response,collection_name,name)
-    result = response.slice(collection_name, :key )
-    result[:doc_count] = result[collection_name][:doc_count]
-    result[:results] = result.delete(collection_name)[name][:buckets]
-    result
-  end
-
 
 end
