259cdee0ba6d45c14a2dd93e77260f97c8dc4ead
georgiana-b <bere.georgi@gmail.com>
2016-05-02 05:08:04 +0300

    Fix update and destroy user account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
+  skip_before_filter :authenticate_scope!, only: [ :update, :destroy]
+  before_filter :authenticate_user!, only: [ :update, :destroy]
   clear_respond_to
   respond_to :json
+
+  def destroy
+    current_user.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(:user)
+    head :no_content
+  end
+
 end
