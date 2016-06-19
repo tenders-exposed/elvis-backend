271947efe652ca68d1be66ce69810f955ed94db5
Georgiana Bere <bere.georgi@gmail.com>
2016-06-19 18:14:05 +0300

    Changed deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :domain, 'toto.tenders.exposed'
 set :deploy_to, '/srv/elvis/staging/backend'
 set :repository, 'https://github.com/tenders-exposed/elvis-backend.git'
-set :branch, 'master'
+set :branch, 'develop'
 set :user, 'elvis'
 set :rails_env, 'staging'
