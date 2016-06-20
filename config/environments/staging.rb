d00c4770650476f2d4a08cebf2fb8ba54b677ea9
Georgiana Bere <bere.georgi@gmail.com>
2016-06-21 00:10:24 +0300

    Change host for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: "elvis.tenders.exposed" }
+  config.action_mailer.default_url_options = { host: "staging.elvis.tenders.exposed" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "mail.tenders.exposed",
