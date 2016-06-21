0045ab68ce9c72e00ba0e29b5fe97601765fdada
Georgiana Bere <bere.georgi@gmail.com>
2016-06-21 03:13:56 +0300

    Handle bad payload for actor details

--- app/controllers/api/v1/contracts/suppliers_controller.rb
+++ app/controllers/api/v1/contracts/suppliers_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Contracts::SuppliersController < Api::V1::ApiController
 
   def details
     details = get_supplier_details
-    render json: search_json_response(count: query[:suppliers].size,
+    render json: search_json_response(count: query.fetch(:suppliers, []).size,
      results: details), status: 200
    rescue => e
      render_error(e.message)
@@ -14,14 +14,14 @@ class Api::V1::Contracts::SuppliersController < Api::V1::ApiController
   end
 
   def query
-    suppliers_params.fetch(:query, {})
+    query = suppliers_params.fetch(:query, {})
   end
 
   def get_supplier_details
     details = []
-     query[:suppliers].each do |x_slug_id|
-      query = Search::Query.new(query.except(:suppliers), suppliers: [x_slug_id])
-      details << Search::ActorDetails.new(query, x_slug_id, "suppliers").details
+     query.fetch(:suppliers, []).each do |x_slug_id|
+      query_object = Search::Query.new(query.except(:suppliers), suppliers: [x_slug_id])
+      details << Search::ActorDetails.new(query_object, x_slug_id, "suppliers").details
     end
     details
   end
