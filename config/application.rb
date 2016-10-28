5984d20a6ecf58350e30d2a61218ca7aaead75b3
Victor Nițu <victor@thesponge.eu>
2016-10-28 16:35:17 +0300

    Allow PUT and PATCH requests

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Elvis
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :options]
       end
     end
 
