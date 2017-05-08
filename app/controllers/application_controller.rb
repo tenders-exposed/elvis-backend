7b9887b034f12d4741b14198d438d52377e4bc1d
Georgiana Bere <bere.georgi@gmail.com>
2017-05-08 18:10:46 +0300

    Allow empty array value for array params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   acts_as_token_authentication_handler_for User, fallback: :none
+  before_filter :fix_json_params
+
+  private
+
+  def fix_json_params
+    if request.format.json?
+      body = request.body.read
+      request.body.rewind
+      params.merge!(ActiveSupport::JSON.decode(body)) unless body == ""
+    end
+  end
 end
