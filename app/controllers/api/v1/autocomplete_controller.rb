c1fd1e379a2a45869a64ce1832ce96df183d7d9a
Georgiana Bere <bere.georgi@gmail.com>
2017-06-12 14:11:34 +0300

    Specify ElasticSearch host in env

--- app/controllers/api/v1/autocomplete_controller.rb
+++ app/controllers/api/v1/autocomplete_controller.rb
@@ -28,9 +28,10 @@ class Api::V1::AutocompleteController < Api::V1::ApiController
   protected
 
   def curated_params
+    int_suggestions = autocomplete_params.fetch(:max_suggestions, nil).to_i
     {
       text: autocomplete_params.fetch(:text, nil).to_s,
-      max_suggestions: autocomplete_params.fetch(:max_suggestions, nil).to_i | 10
+      max_suggestions: int_suggestions > 0 ? int_suggestions : 10
     }
   end
 end
--- config/initializers/mongoid-elasticsearch.rb
+++ config/initializers/mongoid-elasticsearch.rb
@@ -1,5 +1,6 @@
 # Put here because of https://github.com/rs-pro/mongoid-elasticsearch/issues/11
 Mongoid::Elasticsearch.autocreate_indexes = false
+Mongoid::Elasticsearch.client_options[:host] = ENV["ES_HOST"] || 'localhost'
 Contract
 Supplier
 ProcuringEntity
