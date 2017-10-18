422821229c65c6b7233bbdde8a39126623331bb5
Georgiana Bere <bere.georgi@gmail.com>
2017-10-18 17:13:53 +0300

    Provide average competition for all nodes

--- app/services/vis/node.rb
+++ app/services/vis/node.rb
@@ -4,12 +4,13 @@ class Vis::Node
 # FIXME:Remove me after the user has the ability to choose his own colors
   COLORS = {procuring_entity: 'rgb(246, 49, 136)', supplier: 'rgb(36, 243, 255)'}
 
-  def initialize(id, label, value, type, flags = {})
+  def initialize(id, label, value, type, average_competition, flags = {})
     @id = id
     @value = value
     @label = label
     @type = type
     @color = COLORS[@type.to_sym]
+    @average_competition = average_competition
     @flags = flags.slice(:average_competition)
   end
 
